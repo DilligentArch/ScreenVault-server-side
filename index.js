e23c4129fa8af25ee45e4c28b8705025dc9ee97d
unknown <nayebqureshi4@gmail.com>
2024-12-15 18:48:16 +0600

    done

--- index.js
+++ index.js
@@ -72,7 +72,25 @@ async function run() {
 
 
     app.put("/reviews/:id", async (req, res) => {
-   
+      const id = req.params.id;
+      const review = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateReview = {
+        $set: {
+          coverImage: review.coverImage,
+          gameTitle: review.gameTitle,
+          description: review.description,
+          rating: review.rating,
+          year: review.year,
+          genre: review.genre,
+          userEmail: review.userEmail,
+          userName: review.userName,
+        },
+      };
+
+      
     });
 
 
